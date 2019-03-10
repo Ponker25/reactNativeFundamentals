e33acaf71e094d8c3206ec54932b817f90683e72
William Andrew Swensen <williamswensen86@gmail.com>
2019-03-10 15:31:11 -0600

    adding youtube api

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Home } from './app/views/Home';
 import { Contact } from './app/views/Contact';
 import { createAppContainer, createStackNavigator } from 'react-navigation';
+import { Video } from './app/views/Video.js'
 
 const MyRoutes = createStackNavigator(
   {
@@ -11,6 +12,9 @@ const MyRoutes = createStackNavigator(
     },
     ContactRT: {
       screen: Contact
+    },
+    LessonsRT: {
+      screen: Video
     },
 
   },
